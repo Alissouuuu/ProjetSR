1d0f3cd945859d88f702bca3182abaac17c2e579
Alissouuuu <127485296+Alissouuuu@users.noreply.github.com>
2025-03-31 13:58:29 +0200

    Add files via upload

--- login.php
+++ login.php
@@ -5,22 +5,24 @@
     $numCarte = shell_exec('/home/pi/script/tag_detect.sh');
     $_SESSION['numCarte'] = $numCarte;
 
-    // Connexion :
-    require_once("param.inc.php");
-    $mysqli = new mysqli($host, $login, $passwd, $dbname);
-    if ($mysqli->connect_error) {
-        die('Erreur de connexion (' . $mysqli->connect_errno . ') '
-                . $mysqli->connect_error);
-    }
+    echo "<h1>$numcarte</h1>"
 
-    $stmt = $mysqli->prepare("SELECT * FROM votant WHERE numCarte = ? ");
-    $stmt->bind_param("s", $numCarte);
+    // // Connexion :
+    // require_once("param.inc.php");
+    // $mysqli = new mysqli($host, $login, $passwd, $dbname);
+    // if ($mysqli->connect_error) {
+    //     die('Erreur de connexion (' . $mysqli->connect_errno . ') '
+    //             . $mysqli->connect_error);
+    // }
 
-    if ($stmt->execute()){
+    // $stmt = $mysqli->prepare("SELECT * FROM votant WHERE numCarte = ? ");
+    // $stmt->bind_param("s", $numCarte);
 
-        $result = $stmt->get_result()->fetch_assoc();
-        $_SESSION['votant'] = $result;
-        header('Location: vote.php');
-        exit();
-    }
+    // if ($stmt->execute()){
+
+    //     $result = $stmt->get_result()->fetch_assoc();
+    //     $_SESSION['votant'] = $result;
+    //     // header('Location: vote.php');
+    //     exit();
+    // }
 ?>
